c63e35d23b5d8580b8da1b842c3017b2a4826211
Carolina Salazar <cindy.s.r.28@hotmail.com>
2021-01-14 12:02:20 -0500

    pass the provider for global store

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { TestimonyProvider } from './store/TestimonyContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TestimonyProvider>
+      <App />
+    </TestimonyProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
