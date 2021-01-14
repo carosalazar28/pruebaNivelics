8f043484259a80caa024dbb3e3ca20928459037d
Carolina Salazar <cindy.s.r.28@hotmail.com>
2021-01-14 10:57:15 -0500

    created the container for put the styled grid

---
+++ src/components/ContainerFooter.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const ContainerFooter = styled.div ` 
+  display: grid;
+  padding: 40px 30px;
+  grid-gap: 60px;
+  grid-template-areas:
+    "bodyLeft bodyRight";
+  margin: auto;
+  min-width: 1021px;
+`;
