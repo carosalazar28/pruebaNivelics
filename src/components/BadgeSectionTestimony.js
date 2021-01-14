f2c4834a51e3557829446a0c9f8b1a952d631c66
Carolina Salazar <cindy.s.r.28@hotmail.com>
2021-01-14 10:56:51 -0500

    pass the styles for render data

--- src/components/BadgeSectionTestimony.js
+++ src/components/BadgeSectionTestimony.js
@@ -1,23 +1,55 @@
+import styled from 'styled-components';
 import { BadgeTestimony } from './BadgeTestimony';
+import { BadgeNewTestimony } from './BadgeNewTestimony';
+import { ContainerFooter } from './ContainerFooter';
+
+const NameSection = styled.h4`
+  font-size: 34px;
+  text-align: center;
+  margin: 0;
+  padding: 15px;
+`;
+
+const ContainerTestimony = styled.section`
+  background: #1f2126;
+  color: white;
+  margin: 10px auto;
+  position: relative;
+  padding: 17px;
+`;
+
+const BodyLeft = styled.div`
+  grid-area: bodyLeft;
+`;
+
+const TitleTestimony = styled.h5`
+  font-size: 20px;
+`;
 
 export function BadgeSectionTestimony({title, testimonies}) {
   return (
     <>
-      <h4>Testimonios</h4>
-      <section>
-        <h5>{title}</h5>
-        {!!testimonies && testimonies.length > 0 && testimonies.map(({name, title, text, image, service_image}, index) => {
-        return (
-        <BadgeTestimony
-          key={index}
-          name={name}
-          title={title}
-          text={text}
-          image={image}
-          service_image={service_image}
-        />
-        )})}
-      </section>
+    <ContainerFooter>
+      <BodyLeft>
+        <NameSection>Testimonios</NameSection>
+          <hr/>
+        <ContainerTestimony>
+          <TitleTestimony>{title}</TitleTestimony>
+          {!!testimonies && testimonies.length > 0 && testimonies.map(({name, title, text, image, service_image}, index) => {
+          return (
+          <BadgeTestimony
+            key={index}
+            name={name}
+            title={title}
+            text={text}
+            image={image}
+            service_image={service_image}
+          />
+          )})}
+        </ContainerTestimony>
+      </BodyLeft>
+      <BadgeNewTestimony/>
+    </ContainerFooter>
     </>
   )
-}
+};
--- src/components/BadgeTestimony.js
+++ src/components/BadgeTestimony.js
@@ -1,11 +1,25 @@
+import styled from 'styled-components';
+
+const ContainerArticle = styled.article`
+  display: flex;
+  flex-direction: column;
+`;
+
+const ContainerImages = styled.div`  
+  background-color: #fff;
+  border-radius: 10px;
+  display: flex;
+  justify-content: space-around;
+`;
+
 export function BadgeTestimony({name, title, text, image, service_image}) {
   return (
     <>
-    
-        <article>
-          <p>{name}</p>
-          <h6>{title}</h6>
-          <p>{text}</p>
+      <ContainerArticle>
+        <p>{name}</p>
+        <h6>{title}</h6>
+        <p>{text}</p>
+        <ContainerImages>
           <img
             src={image}
             alt="Imagen del servicio"
@@ -14,8 +28,8 @@ export function BadgeTestimony({name, title, text, image, service_image}) {
             src={service_image}
             alt="Icono del servicio"
           />
-        </article>
-
+        </ContainerImages>
+      </ContainerArticle>
     </>
   )
 };
