10c0528c3fdf26779ca3726d8983473c922d5ac4
Carolina Salazar <cindy.s.r.28@hotmail.com>
2021-01-14 10:56:23 -0500

    created the component for render the services and put styles

--- src/components/BadgeSectionService.js
+++ src/components/BadgeSectionService.js
@@ -10,6 +10,25 @@ const NameSection = styled.h4`
 
 const Section = styled.section`
   padding: 0 30px;
+  display: grid;
+  grid-template-columns: repeat(2, minmax(100px, 500px));
+  justify-content: center;
+`;
+
+const Image = styled.img`
+  width: 80px;
+  border-radius: 50%;
+  height: 80px;
+  object-fit: cover;
+`;
+
+const ContainerCategory = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  padding: 17px;
+  background: linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.6));
+  color: white;
 `;
 
 export function BadgeSectionService({name, icon, services}) {
@@ -18,14 +37,14 @@ console.log(services)
     <>
       <NameSection>Servicios</NameSection>
       <hr/>
-      <Section>
-        <div>
-          <h5>{name}</h5>
-          <img
+        <ContainerCategory>
+          <Image
             src={icon}
             alt="Icono logo de la categoria"
-          />
-        </div>
+            />
+          <h5>{name}</h5>
+        </ContainerCategory>
+        <Section>
         {!!services && services.length > 0 && services.map(({id, name, summary, short_description, image, icon, price}) => {
           return(
             <BadgeService
--- src/components/BadgeService.js
+++ src/components/BadgeService.js
@@ -1,18 +1,49 @@
+import styled from 'styled-components';
+
+const ContainerService = styled.article`
+  position: relative;
+  display: block;
+`;
+
+const IconService = styled.img`
+  width: 80px;
+  height: 80px;
+  border-radius: 10px;
+  object-fit: cover;
+  position: relative;
+  top: -15%;
+  right: -9%;
+`;
+
+const ImageService = styled.img`
+  width: 50%;
+`;
+
+const TitleService = styled.h6`
+  font-size: 20px;
+`;
+
+const ContainerText = styled.div`
+  position: relative;
+`;
+
 export function BadgeService({name, summary, description, image, icon, price}) {
   return (
-    <article>
-      <h6>{name}</h6>
-      <p>{summary}</p>
-      <p>{description}</p>
-      <img
+    <ContainerService>
+      <TitleService>{name}</TitleService>
+      <ContainerText>
+        <p>{summary}</p>
+        <p>{description}</p>
+      </ContainerText>
+      <ImageService
         src={image}
         alt="Imagen del servicio"
       />
-      <img
+      <IconService
         src={icon}
         alt="Icono del servicio"
       />
       <p>{price}</p>
-    </article>
+    </ContainerService>
   )
 };
